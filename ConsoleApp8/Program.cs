6baff4f593c03e9cd5539843948e9ea0cc1b96b8
user <user@DESKTOP-22TI95N>
2025-02-08 17:41:11 +0530

    see at you

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world this rohith from github");
+            Console.WriteLine("hello world this rohith from github hi");
 
         }
     }
